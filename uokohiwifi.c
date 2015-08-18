05ebb0456d5d096911898da3e2e7acae0ed22a39
cheng cheng <c.j.king@163.com>
2015-08-18 14:10:40 +0800

    create

--- uokohiwifi.c
+++ uokohiwifi.c
@@ -13,7 +13,7 @@ int main(int argc, char *const *argv)
 {
 	if(get_options(argc, argv) != OK)
 	{
-	return 1;		
+	   return 1;		
 	}
 
 	if(update_uoko_ad)
@@ -36,7 +36,7 @@ get_options(int argc, char *const *argv)
 
         if (*p++ != '-') 
         {
-            printf("invalid option: \"%s\"", argv[i]);
+            printf("invalid option: \"%s\"\n", argv[i]);
             return ERROR;
         }
 
@@ -49,28 +49,28 @@ get_options(int argc, char *const *argv)
                 if (*p) 
                 {
                     usignal = (char *) p;
-
                 }
                 else if (argv[++i]) 
                 {
                     usignal = argv[i];
-
                 } 
                 else 
                 {
-                    printf(0, "option \"-s\" requires parameter");
+                    printf(0, "option \"-s\" requires parameter\n");
                     return ERROR;
                 }
 
+                printf("%d\n", uoko_strcmp(usignal, "update"));
                 if (uoko_strcmp(usignal, "update") == 1)
                 {
                 	update_uoko_ad = 1;
+                    goto next;
                 }
 
                 break;
 
             default:
-                printf(0, "invalid option: \"%c\"", *(p - 1));
+                printf(0, "invalid option: \"%c\"\n", *(p - 1));
                 return ERROR;
             }
         }
