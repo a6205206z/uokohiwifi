6d4a86415209784d587e5b9c1f3b77f746f77548
cheng cheng <c.j.king@163.com>
2015-08-18 13:36:10 +0800

    create

---
+++ uokohiwifi.c
@@ -0,0 +1,81 @@
+
+#include <uokohiwifi.h>
+
+static uint select_option
+static uint update_uoko_ad
+
+
+static int get_options(int argc, char *const *argv)
+
+
+int main(int argc, char *const *argv)
+{
+	if(get_options != OK)
+	{
+	return 1;		
+	}
+
+	if(update_uoko_ad)
+	{
+		printf("update ad \n");		
+	}
+}
+
+
+static int
+get_options(int argc, char *const *argv)
+{
+    u_char     *p;
+    uint   		i;
+
+    for (i = 1; i < argc; i++) {
+
+        p = (u_char *) argv[i];
+
+        if (*p++ != '-') {
+            printf("invalid option: \"%s\"", argv[i]);
+            return ERROR;
+        }
+
+        while (*p) 
+        {
+
+            switch (*p++) 
+            {
+            case 's':
+                if (*p) 
+                {
+                    usignal = (char *) p;
+
+                }
+                else if (argv[++i]) 
+                {
+                    usignal = argv[i];
+
+                } 
+                else 
+                {
+                    printf(0, "option \"-s\" requires parameter");
+                    return ERROR;
+                }
+
+                if (uoko_strcmp(usignal, "update") == 1)
+                {
+                	update_uoko_ad = 1;
+                }
+
+                break;
+
+            default:
+                printf(0, "invalid option: \"%c\"", *(p - 1));
+                return ERROR;
+            }
+        }
+
+    next:
+
+        continue;
+    }
+
+    return OK;
+}
---
+++ uokohiwifi.h
@@ -0,0 +1,12 @@
+#define uint unsigned int
+#define OK 1
+#define ERROR 0
+
+
+#define uoko_strcmp(s1, s2)  strcmp((const char *) s1, (const char *) s2)
+
+
+
+
+
+static int get_options(int argc, char *const *argv)
