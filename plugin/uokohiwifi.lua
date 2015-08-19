ab4c99b7db1f390f0edb5a494df41c58ebacbdf9
cheng cheng <c.j.king@163.com>
2015-08-19 10:25:52 +0800

    fix

--- plugin/uokohiwifi.lua
+++ plugin/uokohiwifi.lua
@@ -5,25 +5,46 @@ local UOKO_DIR_PATH = "/www/uoko"
 local UOKO_AD_LOCAL_DIR_PATH = "/www/uoko/ad" 
 local UOKO_AD_LOCAL_FILE_PATH = "/www/uoko/add/ad.html"
 
+function file_exists(filepath)
+  local f = io.open(filepath, "r")
+  if f then
+    f:close()
+    return true
+  else
+    return false
+  end
+end
+
+function mkdirr(dirpath)
+  os.execute("mkdir -p '" .. dirpath .. "' -m777")
+end
+
 function download_ad_page()
 	if not file_exists(UOKO_DIR_PATH) then
-		local mkdircmd = string.format("mkdir %s",UOKO_DIR_PATH)
-		os.execute(mkdircmd)
+		mkdirr(UOKO_DIR_PATH
 	end
 
 	if not file_exists(UOKO_AD_LOCAL_DIR_PATH) then
-		local mkdircmd = string.format("mkdir %s",UOKO_AD_LOCAL_DIR_PATH)
-		os.execute(mkdircmd)
+		mkdirr(UOKO_AD_LOCAL_DIR_PATH)
 	end
 
 	local cmd = string.format("wget -O %s %s", UOKO_AD_LOCAL_FILE_PATH, UOKO_AD_SERVICE_URL)
 	os.execute(cmd)
+
+	return 1,"download"
 end
 
 
 
-if arg[1] == "-u" then
+if arg[1] == "update" then
 	if arg[2] == "ad" then
-		download_ad_page()
-	end	
+		rc,msg = download_ad_page()
+		print(msg)
+		os.exit(rc)
+	end
+else
+	print("Usage:")
+	print(" uokohiwifi -u    	        -- update")
+	print("					  ad 	    -- update uoko ad to local server")
+	os.exit(2)
 end
