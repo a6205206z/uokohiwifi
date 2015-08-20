360e2fc8b384718b47628f530769fe6f8d4e141b
cheng cheng <c.j.king@163.com>
2015-08-20 09:26:32 +0800

    fix

---
+++ os/tools/remotecmd.c
@@ -0,0 +1,88 @@
+include "remotecmd.h"
+
+
+static uint         select_option;
+static char         *usignal;
+static const char   *uoko_command_server = "http://www.baidu.com/";
+
+static int get_options(int argc, char *const *argv);
+static int pull_command();
+
+
+int main(int argc, char *const *argv)
+{
+	if(get_options(argc, argv) != OK)
+	{
+	   return 1;		
+	}
+}
+
+
+static int pull_command()
+{
+    return OK;
+}
+
+
+
+static int
+get_options(int argc, char *const *argv)
+{
+    u_char     *p;
+    uint   		i;
+
+    for (i = 1; i < argc; i++) 
+    {
+
+        p = (u_char *) argv[i];
+
+        if (*p++ != '-') 
+        {
+            printf("invalid option: \"%s\"\n", argv[i]);
+            return ERROR;
+        }
+
+        while (*p) 
+        {
+
+            switch (*p++) 
+            {
+            case 's':
+                if (*p) 
+                {
+                    usignal = (char *) p;
+                }
+                else if (argv[++i]) 
+                {
+                    usignal = argv[i];
+                } 
+                else 
+                {
+                    printf(0, "option \"-s\" requires parameter\n");
+                    return ERROR;
+                }
+
+
+                if (uoko_strcmp(usignal, "--pull-command") == 0)
+                {
+                    pull_command();
+                    goto next;
+                }
+
+                printf(0, "invalid option: \"-s %s\"", usignal);
+                return ERROR;
+                break;
+
+            default:
+                printf(0, "invalid option: \"%c\"\n", *(p - 1));
+                return ERROR;
+            }
+        }
+
+    next:
+
+        continue;
+    }
+
+    return OK;
+}
---
+++ os/tools/remotecmd.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <linux/in.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define uint unsigned int
+#define OK 1
+#define ERROR 0
+
+
+#define uoko_strcmp(s1, s2)  strcmp((const char *) s1, (const char *) s2)
+
+
+
