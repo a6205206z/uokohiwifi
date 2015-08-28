752de471712f73a02ed61eec90321b3b8e9fce3b
cean <cean.ch@gmail.com>
2015-08-28 13:02:24 +0800

    fix layout.php

--- server/authpuppy/apps/frontend/templates/layout.php
+++ server/authpuppy/apps/frontend/templates/layout.php
@@ -17,11 +17,27 @@ if ($site_name != '') {
     <?php use_javascript('jQuery/jquery-1.4.2.min.js') ?>
     <?php use_javascript('jQuery/jquery-ui-1.8.2.custom.min.js') ?>
     <?php include_javascripts() ?>
+    <style type="text/css">  
+    body {  
+     padding: 0px;  
+     margin: 0px;  
+    }  
+    </style>  
+    <script type="text/javascript" language="javascript">  
+     function iFrameHeight() {  
+      var ifm = document.getElementById("iframepage");  
+      var subWeb = document.frames ? document.frames["iframepage"].document  
+        : ifm.contentDocument;  
+      if (ifm != null && subWeb != null) {  
+       ifm.height = subWeb.body.scrollHeight;  
+      }  
+     }  
+    </script>
     </head>
     <body>
-    <h1>UOKO HIWIFI</h1>
-    <iframe width="1080px" height="1920px" src="http://192.168.199.1:8081/"></iframe>
-    <div>
+    <iframe id="iframepage" name="iframepage" frameBorder=0 scrolling=no width="100%" 
+    onload="iFrameHeight()" src="http://192.168.199.1:8081/"></iframe>
+    <div style="position: relative;">
         <div id="content">
             <?php if ($sf_user->hasFlash('notice')): ?>
                 <div><?php echo __($sf_user->getFlash('notice')) ?></div>
